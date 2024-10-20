91a292d8df43e2bbb69e8d8368ed40a27f3e85b1
Nataliia-Opanasenko <146832931+Nataliia-Opanasenko@users.noreply.github.com>
2024-10-20 14:28:24 +0200

    delete import logo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import logo from './logo.svg';
+
 import './App.css';
 import WeatherData from "./WeatherData";
 import Footer from './Footer';
-import axios from "axios";
 import Header from './Header';
 
 
